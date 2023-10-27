e9ba47034090c6d25992834325a71a0ece3a6392
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2023-10-27 02:55:39 +0200

    Remove starter-topic.md and update documentation configuration
    This commit includes the removal of the starter-topic.md file as it was no longer needed. It was initially used as a template file for the new documentation, but its purpose has been served and is now irrelevant. The writerside.cfg file has also been modified to reflect the version number change to v1.1.3, and a new code-snippets directory is added. Also, buildprofiles.xml now has a more descriptive build profile with added attributes for color presets, copyright and social media links. A new file, redirection-rules.xml, is introduced to handle redirection rules for the deleted page. Finally, a comprehensive README.md file has been included in the topics, replacing the starter-topic.md as the index file, providing more relevant content. Overall, these changes improve the organization, relevancy and navigability of the project's documentation.

---
+++ Documentation/code-snippets/make.ps1
@@ -0,0 +1,55 @@
+param (
+    [Parameter(Position=0, Mandatory=$true)]
+    [string]$action
+)
+
+$Version = "1.1.3"
+$BuildID = Get-Date -Format "Hmmss"
+
+function build {
+    clean
+    Write-Host "Starting build..."
+    go mod tidy
+    Write-Host "Generating binary icon"
+    rsrc -arch amd64 -ico .\internal\trayMenu\icon.ico
+    Write-Host "Generating binary version info"
+    goversioninfo -64 -product-ver-build $BuildID -ver-build $BuildID
+    Write-Host "Building project"
+    go build -ldflags "-extldflags '-static' -w -s -H windowsgui -X main.AppVersion=$Version -X main.BuildID=$BUILDID" `
+        -o .\bin\TextType.exe .
+    Write-Host "Build completed."
+}
+
+function clean {
+    Write-Host "Cleaning project"
+    go clean
+    Write-Host "Cleaning binary folder"
+    if (Test-Path .\bin) {
+        Remove-Item -Force -Recurse .\bin
+    } else {
+        Write-Host "No binary folder to clean."
+    }
+    Write-Host "Cleaning resources folder completed."
+}
+
+function dep {
+    Write-Host "Installing dependencies"
+    go get github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+    go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+    go get github.com/akavel/rsrc
+    go install github.com/akavel/rsrc
+    go get .
+    go mod tidy
+}
+
+switch ($action) {
+    'build' {
+        build
+    }
+    'clean' {
+        clean
+    }
+    default {
+        Write-Host "Invalid action parameter. Use 'build' or 'clean'."
+    }
+}
