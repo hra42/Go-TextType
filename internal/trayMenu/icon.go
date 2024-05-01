8189ff4c20d79191b813f89a70b95462b9966ec8
HRA42 <44985923+HRA42@users.noreply.github.com>
2024-05-01 23:06:19 +0200

    Refactor readIcon function for better error handling in icon.go
    Signed-off-by: HRA42 <44985923+HRA42@users.noreply.github.com>

--- internal/trayMenu/icon.go
+++ internal/trayMenu/icon.go
@@ -5,7 +5,7 @@ import "embed"
 //go:embed icon.ico
 var Icon embed.FS
 
-func ReadIcon() (data []byte) {
-	data, _ = Icon.ReadFile("icon.ico")
+func readIcon() (data []byte, err error) {
+	data, err = Icon.ReadFile("icon.ico")
 	return
 }
